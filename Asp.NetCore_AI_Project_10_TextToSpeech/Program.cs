c3448c7287561fd54e9c52cd76767c3da759017f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-17 21:56:14 +0300

    Text To Speech Process Added

---
+++ Asp.NetCore_AI_Project_10_TextToSpeech/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer speechSynthesizer = new();
+        speechSynthesizer.Volume=100; // Sesin ses seviyesini ayarlıyoruz (0-100)
+        speechSynthesizer.Rate = 0; // Sesin hızını ayarlıyoruz (-10 ile 10 arasında)
+        Console.WriteLine("Lütfen Seslendirmek İstediğiniz Cümleyi Giriniz..");
+        string inputText = Console.ReadLine(); // Kullanıcıdan cümle alıyoruz
+        if(!string.IsNullOrEmpty(inputText)) // Eğer cümle boş değilse
+        {
+            speechSynthesizer.Speak(inputText); // Cümleyi seslendiriyoruz
+            Console.ReadLine(); // Kullanıcının bir tuşa basmasını bekliyoruz
+        }
+        else
+        {
+            Console.WriteLine("Boş bir cümle girdiniz. Lütfen geçerli bir cümle girin."); // Hata mesajı
+        }
+    }
+}
